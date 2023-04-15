1e94cf5f967f3f25f911a734386e8b0915e4adcf
Alperen Aga <alperencode@gmail.com>
2023-04-15 09:39:38 +0300

    Add MsAccess/Query(26-27)

---
+++ MSAccess/Query26.sql
@@ -0,0 +1,13 @@
+﻿SELECT KÖ.ad AS Öğretmen
+	,KÖ.soyAd AS Ad
+	,KZB.ad AS ZümreBşk
+	,KZB.soyad AS Adı
+
+FROM öğretmen AS Ö
+	,öğretmen AS ZB
+	,kullanıcı AS KÖ
+	,kullanıcı AS KZB
+
+WHERE Ö.zümreBşk = ZB.kullanıcıKodu
+	And Ö.kullanıcıKodu = KÖ.kod
+	And ZB.kullanıcıKodu = KZB.kod;
---
+++ MSAccess/Query27.sql
@@ -0,0 +1,9 @@
+﻿SELECT A.ad
+	,A.soyad
+
+FROM kullanıcı AS A
+	,kullanıcı AS B
+
+WHERE A.dTarihi > B.dTarihi
+	And B.ad = 'Ayşe'
+	And B.Soyad = 'Dalgıç';
